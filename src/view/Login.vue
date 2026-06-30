d06ad8981ce0c9229e59bffd9ce3863191473cf4
liubinglin <2624560037@qq.com>
2026-06-30 17:16:17 +0800

    欢迎页面

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -50,7 +50,7 @@ const form = ref({
 
 const rules = {
   username: [
-    {required: true, message: '请输入用户名sges', trigger: 'blur'}
+    {required: true, message: '请输入用户名', trigger: 'blur'}
   ],
   password: [
     {required: true, message: '请输入密码', trigger: 'blur'},
